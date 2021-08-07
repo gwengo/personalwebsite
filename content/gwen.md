96d3ff9a8b2f0cfd9ff71f6d78a00eb7ec62462c
gwengo <gwen.goins@gmail.com>
2021-08-07 02:17:42 -0400

    other files

---
+++ content/gwen.md
@@ -0,0 +1,10 @@
+Title: Gwen
+Date: 07-07-2021
+Category: gwen
+Slug: gwen
+Authors: gwen
+Summary: gwen
+
+# gwen
+
+gwen!
---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+# todo - for gwen
+# change theme
+AUTHOR = "gwen goins"
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+CATEGORY_FEED_ATOM = None
+DEFAULT_DATE_FORMAT = "%a %B %d %Y"
+DEFAULT_LANG = "en"
+DEFAULT_PAGINATION = False
+DELETE_OUTPUT_DIRECTORY = True
+DISPLAY_CATEGORIES_ON_MENU = True
+DISPLAY_PAGES_ON_MENU = True
+FEED_ALL_ATOM = None
+LOAD_CONTENT_CACHE = False
+OUTPUT_PATH = "output/"
+PATH = "./content"
+RELATIVE_URLS = False  # document-relative URLs when developing
+SITENAME = "gwen goins"
+SITEURL = "https://gwengoins.com"
+# todo - change to the theme you're using
+THEME = "theme/taman/"
+THEME_STATIC_DIR = "theme"
+TIMEZONE = "America/New_York"
+TRANSLATION_FEED_ATOM = None
+USE_FOLDER_AS_CATEGORY = True
+
+# LINKS = ()
+EXTRA_PATH_METADATA = {
+    # "assets/favicon.ico": {"path": "favicon.ico"},
+}
+PAGE_PATHS = ["pages"]
+PLUGINS = []
+STATIC_PATHS = ["assets"]
+THEME_STATIC_PATHS = ["static"]
